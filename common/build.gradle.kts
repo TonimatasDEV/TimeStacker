124a13c9e20199959fed6439371645a972fc7482
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-05-03 17:33:47 +0200

    Fix #9

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,6 +4,11 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:$fabricLoaderVersion")
 }
 
+loom {
+    @Suppress("UnstableApiUsage")
+    mixin.defaultRefmapName.set("timestacker-common.refmap.json")
+}
+
 architectury {
     common("fabric", /*"forge",*/ "neoforge")
 }
