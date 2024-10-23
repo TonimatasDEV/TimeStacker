1c11ba62d046acc11b8679cd877b81e4fbdeeb14
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-10-23 19:53:20 +0200

    Port to 1.21.3

--- common/src/main/java/dev/tonimatas/timestacker/mixins/AreaEffectCloudMixin.java
+++ common/src/main/java/dev/tonimatas/timestacker/mixins/AreaEffectCloudMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AreaEffectCloud.class)
 public class AreaEffectCloudMixin {
-    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z"))
+    @Redirect(method = "serverTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z"))
     public boolean finishUsing(LivingEntity livingEntity, MobEffectInstance mobEffectInstance, Entity entity) {
         return TimeStacker.applyEffect(livingEntity, mobEffectInstance, entity);
     }
